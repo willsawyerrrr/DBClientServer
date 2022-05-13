13fc6fbec69d5c846d6a45b21a291a15435b4b5a
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-05-13 15:53:33 +0000

    Initialised simple pseudo-code for operation
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a4@156 a1968c47-03bd-fd43-9cb2-c192510db353

---
+++ dbclient.c
@@ -0,0 +1,14 @@
+#include "dbclient.h"
+
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    // validate command line arguments - exit if appropriate
+
+    // connect to server
+
+    // GET or PUT key/value pair
+
+    return EXIT_SUCCESS;
+}
+
