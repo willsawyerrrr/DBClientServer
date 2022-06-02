70d80a140e3599b2f05ce245f9a09ed71170c1cf
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-06-02 10:04:00 +0000

    Improved function comment
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a4@181 a1968c47-03bd-fd43-9cb2-c192510db353

--- request.h
+++ request.h
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/* construct_HTTP_request()
+ * ------------------------
  * Returns a malloc()-allocated buffer containing the HTTP request appropriate
  * for the given action, database, key and value.
+ *
+ * action: what this request is trying to do
+ * database: which database to interact with
+ * key: key to query or write to
+ * value: if present, value to write to given key
  *
  * It is the caller's responsibility to free this buffer.
  */
